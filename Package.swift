1bd0b19220538e28cd8766ce8bcb7f0b2857bd5e
autoafterdark <85806085+autoafterdark@users.noreply.github.com>
2021-12-09 20:32:47 -0700

    Retry Swift Changes

--- Package.swift
+++ Package.swift
@@ -1,15 +1,15 @@
 // swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
-import SwiffyPackage
+import Swift
 
 let package = Swift(
-    name: "Swift Code",
+    name: "Swift",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Swift Code",
-            targets: ["Swift Code"]),
+            name: "Swift",
+            targets: ["Swift"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Swift(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Swift Code",
+            name: "Swift",
             dependencies: [XLibraryCodeTests]),
         .testTarget(
             name: "XLibraryCodeTests",
-            dependencies: ["Swift Code"]),
+            dependencies: ["Swift"]),
     ]
 )
